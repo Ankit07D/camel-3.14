10a03b4ace09dd6b562f6b073224522db9ba5999
Ankit Dubey <andubey@redhat.com>
2022-08-27 19:43:27 +0530

    Initial Commit

---
+++ src/main/java/com/redhat/camel_sample/MySpringBootRouter.java
@@ -0,0 +1,26 @@
+package com.redhat.camel_sample;
+
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+/**
+ * A simple Camel route that triggers from a timer and calls a bean and prints to system out.
+ * <p/>
+ * Use <tt>@Component</tt> to make Camel auto detect this route when starting.
+ */
+@Component
+public class MySpringBootRouter extends RouteBuilder {
+
+    @Override
+    public void configure() {
+        from("timer:hello?period={{timer.period}}").routeId("hello")
+            .transform().method("myBean", "saySomething")
+            .filter(simple("${body} contains 'foo'"))
+                .to("log:foo")
+            .end()
+            .log(LoggingLevel.INFO, "hello route", "This is my messahg")
+            .to("stream:out");
+    }
+
+}
